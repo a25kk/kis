3428febe915017f76788900cdffb5b25518893fe
Christoph Böhner-Figas <boehner@team23.de>
2018-11-12 11:15:16 +0100

    [WIP] Python3 support
    Refactor build structure to archieve python 3 code compatibility

--- src/kis.sitetheme/setup.py
+++ src/kis.sitetheme/setup.py
@@ -36,7 +36,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'plone.app.theming',
-        'plone.app.themingplugins',
+        #'plone.app.themingplugins',
         'setuptools',
     ],
     extras_require={
